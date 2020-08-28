10994dd64b540cf54ba8f4697e50b81d6e627b08
EdsonArchela <edi.eeq@gmail.com>
2020-08-28 10:36:20 -0300

    port change

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -32,6 +32,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
+app.listen(3334, () => {
   console.log('⚓Server started on port 3333!');
 });
